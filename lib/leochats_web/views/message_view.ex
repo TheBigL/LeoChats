ca7d7ea446eb2e9e502cf1c4127d4a2bba465157
TheBigL <lmohamed1@studentmail.nait.ca>
2019-07-31 20:12:02 -0600

    Adding the Messages Controoller

---
+++ lib/leochats_web/views/message_view.ex
@@ -0,0 +1,3 @@
+defmodule LeochatsWeb.MessageView do
+  use LeochatsWeb, :view
+end
---
+++ priv/repo/migrations/20190801020136_create_messages.exs
@@ -0,0 +1,13 @@
+defmodule Leochats.Repo.Migrations.CreateMessages do
+  use Ecto.Migration
+
+  def change do
+    create table(:messages) do
+      add :name, :string
+      add :content, :text
+
+      timestamps()
+    end
+
+  end
+end
